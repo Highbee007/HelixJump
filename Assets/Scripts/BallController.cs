4523a2ceaa64dd9456f2e5e809b1b07518bb2651
Nageri Ibrahim <ibrahimdamilola43@gmail.com>
2025-10-24 15:48:24 +0100

    Bounce commit

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+
+    private Rigidbody ballRb;
+    public bool isNextCollision;
+    public float speed = 7f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ballRb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ballRb.AddForce(Vector3.up * speed, ForceMode.Impulse);
     }
 
     // Update is called once per frame
