18d9671a9b5a4dbd7b9b142a7a1be18054253cc5
Nageri Ibrahim <ibrahimdamilola43@gmail.com>
2025-10-25 07:42:42 +0100

    Rise and shine

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player;
+    private BallController ball;
+    private float offset;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player").GetComponent<GameObject>();
+        ball = GameObject.Find("Ball").GetComponent<BallController>();
+        offset = transform.position.y - ball.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 currentPos = transform.position;
+        currentPos.y = ball.transform.position.y + offset;
+        transform.position = currentPos;
     }
 }
